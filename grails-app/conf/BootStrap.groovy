808a57463d6e003a37d0a542db52e1c8054e5162
Daniel Gacitúa <danielgacituav@gmail.com>
2016-03-25 22:59:33 -0300

    Preparación para carga de datos de prueba

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,5 +1,7 @@
 import grails.util.Environment
 import ordertracker.Cliente
+import ordertracker.Marca
+import ordertracker.Producto
 
 class BootStrap {
 	def init = { servletContext ->
@@ -47,20 +49,40 @@ class BootStrap {
 	
 	private void seedDevData() {
 		// TODO Terminar Carga de datos en modo desarrollo
-		def cliente = null
-		println "Iniciando carga de clientes en la Base de Datos"
+		println "Iniciando carga de datos de prueba en la Base de Datos"
 		
-		cliente = new Cliente(apellido: "Gacitúa Vásquez", nombre: "Daniel", email: "dgv@mail.com", codigoUnico: "0", razonSocial: "0", direccion: "Paseo Colón 850", latitud: -34.5887297d, longitud: -58.3966085d)
+		// Ejemplo de inserción de Cliente
+		def cliente = null
+		cliente = new Cliente(apellido: "Gacitúa Vásquez", nombre: "Daniel", email: "dgv@mail.com", razonSocial: "Mi Negocio", direccion: "Paseo Colón 850", latitud: -34.5887297d, longitud: -58.3966085d)
 		assert cliente.save(failOnError:true, flush:true, insert: true)
 		cliente.errors = null
 		
-		cliente = new Cliente(apellido: "Beltrán", nombre: "Belén", email: "bb@mail.com", codigoUnico: "1", razonSocial: "1", direccion: "Av Las Heras 2214", latitud: -34.5884456d, longitud: -58.3960098d)
+		cliente = new Cliente(apellido: "Beltrán", nombre: "Belén", email: "bb@mail.com", razonSocial: "Un Kiosco Cualquiera", direccion: "Av Las Heras 2214", latitud: -34.5884456d, longitud: -58.3960098d)
 		assert cliente.save(failOnError:true, flush:true, insert: true)
 		cliente.errors = null
 		
 		assert Cliente.count == 2;
-		
 		println "Finalizada carga de $Cliente.count clientes en la Base de Datos"
+		
+		// Ejemplo de inserción de Marca
+		def marca = null
+		marca = new Marca(nombre: "SuperFantabuloso")
+		assert marca.save(failOnError:true, flush:true, insert: true)
+		marca.errors = null
+		
+		assert Marca.count == 1;
+		println "Finalizada carga de $Marca.count marcas en la Base de Datos"
+		
+		// Ejemplo de inserción de Producto
+		def producto = null
+		producto = new Producto(nombre: "Clavos Princesa", precio: 49.99, stock: 0)
+		assert producto.save(failOnError:true, flush:true, insert: true)
+		producto.errors = null
+		
+		assert Producto.count == 1;
+		println "Finalizada carga de $Producto.count productos en la Base de Datos"
+		
+		println "Finalizando carga de datos de prueba en la Base de Datos"
 	}
 	
 	private void seedProdData() {
